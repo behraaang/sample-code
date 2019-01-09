cc93b0d0f67d3178f690137d8293822a7a51494e
behraaang <me@behrang.studio>
2019-01-09 21:45:40 +0330

    user recommendation first try.

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,23 @@ class User < ActiveModel
   def favourite_authors_books
     Follow.all.select {|f| f.user == self }.map { |f| f.author.books }.flatten
   end
+
+  def upvote_share_count(user)
+    (upvoted_books & user.upvoted_books).count
+  end
+
+  def upvote_share_weight(user)
+    user.upvoted_books.count != 0 ? upvote_share_count(user) / user.upvoted_books.count : 0
+  end
+
+  def recommended_books_from(user)
+    user.upvoted_books.inject([]) { |arr, book| arr << {book: book, weight: upvote_share_weight(user) } }
+  end
+
+  def recommendation_list
+    array = User.all.inject([]) { |arr, user| arr << recommended_books_from(user) }.flatten
+    grouped_array = array.group_by { |hash| hash.values_at :book }
+    hashes_array  = grouped_array.map { |book, book_hashes| { book: book, weight: book_hashes.inject(0) {|sum, book_hash| sum + book_hash[:weight] } } }
+    hashes_array.sort_by { |book_hash| -book_hash[:weight] }
+  end
 end
