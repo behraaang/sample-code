bc088371bfdab25517ca0cebe3bccd0311f11bbb
behraaang <me@behrang.studio>
2019-01-09 21:49:24 +0330

    remove unnecessary genarators module

--- app/feed.rb
+++ app/feed.rb
@@ -1,31 +1,29 @@
-module Generators
-  class Feed
-    def initialize(user)
-      @user = user
-    end
+class Feed
+  def initialize(user)
+    @user = user
+  end
 
-    def retrieve
-      aggregate_books
-      @user.last_active = Time.now
-      sort_feed(Book.all, @aggregated_books)
-    end
+  def retrieve
+    aggregate_books
+    @user.last_active = Time.now
+    sort_feed(Book.all, @aggregated_books)
+  end
 
-    # Bonus
-    def refresh
-      aggregate_books
-      new_books = Book.from_time(@user.last_active)
-      @user.last_active = Time.now
-      sort_feed(new_books, @aggregated_books)
-    end
+  # Bonus
+  def refresh
+    aggregate_books
+    new_books = Book.from_time(@user.last_active)
+    @user.last_active = Time.now
+    sort_feed(new_books, @aggregated_books)
+  end
 
-    private
+  private
 
-    def aggregate_books
-      @aggregated_books = @user.favourite_authors_books + @user.upvoted_books_authors_books
-    end
+  def aggregate_books
+    @aggregated_books = @user.favourite_authors_books + @user.upvoted_books_authors_books
+  end
 
-    def sort_feed(books, aggregated_books)
-      books.sort { |b| -(aggregated_books.count(b)) }
-    end
+  def sort_feed(books, aggregated_books)
+    books.sort { |b| -(aggregated_books.count(b)) }
   end
 end
